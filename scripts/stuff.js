5d68d8eba8b7d8e0ef644351d995d671024353c9
Jamie Zhou <jamizh@umich.edu>
2024-06-03 00:09:39 -0400

    More CSS

---
+++ scripts/stuff.js
@@ -0,0 +1,9 @@
+const chapters = [
+  {
+    title: "Mission Control Center",
+    html_link: "./html/stuff.html",
+    link: "https://www.youtube.com/embed/CDE8brlaWrQ?si=GLBFrlvMQ1_DD8f1",
+    description:
+      "I’m in this club called the Collaborative Lab for Advancing work in space, and we enter the NASA SUITS challenge, which challenges students to design and develop spacesuit information displays within augmented reality (AR) environments, every year. The Mission Control Center was created for the NASA SUITS challenge. The MCC displays astronaut vitals, location of astronauts and landmarks, communication, and navigation. It is built with React.js on the frontend, and Node.js/Websocket on the backend, receiving telemetry stream–or data–from two Microsoft HoloLens. I worked on the frontend of this project, specifically the search box that filters upon astronaut, waypoint, and geosample names; importing the google maps api and using bootstrap components such as modals to add waypoints. In the club, we’re divided into subteams—so like, an AR, UI/UX, web, hardware, etc—and used a scrum framework. I was scrum master of the vitals features, so I coordinated among all the teams, kept track of everyone’s progress, and kept us focused on the task.",
+  },
+];
